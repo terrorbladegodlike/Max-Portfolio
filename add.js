56aa5a95926d49aa543d9613f85bca8d8d532af5
Mihail <ternovetchii.2002@mail.ru>
2023-03-29 11:35:43 +0300

    added a block skills and js animations

--- add.js
+++ add.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 
+const first__skill = document.querySelector(".skill:first-child");
+const sk__counters = document.querySelectorAll(".counter span");
+const progress__bars = document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll", () => {
+   if (!skillsPlayed) skillsCounter();
+});
+
 /* ---------- Sticky Navbar ---------- */
 
 function stickyNavbar() {
@@ -19,3 +27,44 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info", { delay: 600 });
 sr.reveal(".showcase-image", { origin: "top", delay: 700 });
+
+/* ---------- Skills Progress Bar Animation ---------- */
+
+function hasReached(el) {
+   let topPosition = el.getBoundingClientRect().top;
+
+   if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+   return false;
+}
+
+function updateCount(num, maxNum) {
+   let currentNum = +num.innerText;
+
+   if (currentNum < maxNum) {
+      num.innerText = currentNum + 1;
+      setTimeout(() => {
+         updateCount(num, maxNum);
+      }, 12)
+   }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter() {
+   if (!hasReached(first__skill)) return;
+
+   skillsPlayed = true;
+
+   sk__counters.forEach((counter, i) => {
+      let target = +counter.dataset.target;
+      let strokeValue = 427 - 427 * (target / 100);
+
+      progress__bars[i].style.setProperty("--target", strokeValue);
+
+      setTimeout(() => {
+         updateCount(counter, target);
+      }, 400)
+   });
+
+   progress__bars.forEach((p) => (p.style.animation = "progress 2s ease-in-out forwards"));
+}
