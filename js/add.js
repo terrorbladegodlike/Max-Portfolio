ccd493890d43dfe9cb4d1511c1cb8e013a1bccfe
Mihail <ternovetchii.2002@mail.ru>
2023-03-31 13:03:32 +0300

    added a block testimonials, contact, subscription and footer, also scroll to the desired blocks when clicked, also added a dark theme and a theme change when clicked

--- js/add.js
+++ js/add.js
@@ -14,7 +14,12 @@ const images = document.querySelectorAll(".images img");
 const next__btn = document.querySelector(".next-btn");
 const prev__btn = document.querySelector(".prev-btn");
 
+const links = document.querySelectorAll(".nav-link");
+
+const toggle__btn = document.querySelector(".toggle-btn");
+
 window.addEventListener("scroll", () => {
+   activeLink();
    if (!skillsPlayed) skillsCounter();
    if (!mlPlayed) mlCounter();
 });
@@ -151,3 +156,56 @@ function changeImage(index) {
    images[index].classList.add("showImage");
 }
 
+/* ---------- Slider Animation ---------- */
+
+const swiper = new Swiper('.swiper', {
+   loop: true,
+   speed: 500,
+   autoplay: true,
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+});
+
+/* ---------- Change Active Link On Scroll ---------- */
+
+function activeLink() {
+   let sections = document.querySelectorAll("section[id]");
+   let passedSections = Array.from(sections).map((sct, i) => {
+      return {
+         y: sct.getBoundingClientRect().top - header.offsetHeight,
+         id: i,
+      };
+   }).filter(sct => sct.y <= 0);
+
+   let currSectionID = passedSections.at(-1).id;
+
+   links.forEach(l => l.classList.remove("active"));
+   links[currSectionID].classList.add("active");
+}
+
+activeLink();
+
+/* ---------- Change Page Theme ---------- */
+
+let firstTheme = localStorage.getItem("dark");
+
+changeTheme(+firstTheme);
+
+function changeTheme(isDark) {
+   if (isDark) {
+      document.body.classList.add("dark");
+      toggle__btn.classList.replace("bx-moon", "bx-sun");
+      localStorage.setItem("dark", 1);
+   }
+   else {
+      document.body.classList.remove("dark");
+      toggle__btn.classList.replace("bx-sun", "bx-moon");
+      localStorage.setItem("dark", 0);
+   }
+}
+
+toggle__btn.addEventListener("click", () => {
+   changeTheme(!document.body.classList.contains("dark"));
+});
